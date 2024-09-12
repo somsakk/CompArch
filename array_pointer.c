44a530d3da6ae6e17a532c4cdc807eb07fc989f8
Somsak K <somsakk@siit.tu.ac.th>
2024-09-12 08:10:23 +0700

    Revise files for LN4

--- array_pointer.c
+++ array_pointer.c
@@ -11,21 +11,20 @@ int main(void)
     printf("ar[2] = %d\n", ar[2]);
 
     // we can access array elements via pointers as well
-    printf("ar[0] = %d\n", *ar); // hence, ar[0] is the same as *ar
-    printf("ar[1] = %d\n", *(ar+1)); // ar[1] is the as *(ar+1)
-    printf("ar[2] = %d\n", *(ar+2));
-
+    printf("ar[0] = %d\n", *ar); // hence, ar[0] == *ar
+    printf("ar[1] = %d\n", *(ar+1)); // ar[1] == *(ar+1)
+    printf("ar[2] = %d\n", *(ar+2)); // ar[2] == *(ar+2)
     // Find addresses of array elements
     // %p is used to print out pointer value
     printf("Address of ar[0] is %p\n",  ar);
     printf("Address of ar[1] is %p\n", ar+1);
     printf("Address of ar[2] is %p\n", ar+2);
-    // use & notation
-    printf("Address of ar[0] is %p\n", &ar[0]); // ar is the same as &ar[0]
+    // Equivalently, we can use & notation
+    printf("Address of ar[0] is %p\n", &ar[0]); 
     printf("Address of ar[1] is %p\n", &ar[1]);
     printf("Address of ar[2] is %p\n", &ar[2]);
 
-    // C allows accesing memory outside the array
+    // C allows accesing memory outside the array. 
     printf("ar[3] = %d\n", ar[3]);
     printf("ar[3] = %d\n", *(ar+3));
 }
--- string_pointer_ex.c
+++ string_pointer_ex.c
@@ -2,15 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-    char * name = "Somsak7"; // same as char name[] = "Somsak7";
-    char* ch = name;
-    for(int i = 0; i <= strlen(name); i++) { 
-        printf("%c = %d = %#x,", *ch, *ch, *ch); 
-        printf(" Address of the current character is ch = %p\n", ch);
-        // printf("ch is pointing to %s\n", *ch);
-        ch++;
-    }
+    char *name = "abc1"; // same as char name[] = "abc1";
+    char *ch = name; // ch also points to the first element of the string.
     printf("size of 'name' pointer = %d\n", (int) sizeof(name));  
-    printf("length of %s = %d\n", name, (int) strlen(name));
-    // printf("ch is pointing to %s\n", *ch); // causing segmentation fault.
+    printf("length of %s = %d\n\n", name, (int) strlen(name));
+    
+    for(int i = 0; i <= strlen(name); i++) { 
+        printf("length of %s = %d\n", ch, (int) strlen(ch));
+        printf("%c = %d = %#x,", *ch, *ch, *ch); 
+        printf("\tAddress of this character is %p\n\n", ch);
+        ch++;
+        // equivalent to
+        printf("length of %s = %d\n", &name[i], (int) strlen(&name[i]));
+        // printf("length of %s = %d\n", name+i, (int) strlen(name+i));
+        printf("%c = %d = %#x,", name[i], name[i], name[i]); 
+        printf("\tAddress of this character is %p\n\n", &name[i]);
+        // We can also use (name+i) in place of &name[i]        
+    }
 }
