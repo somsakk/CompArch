84e950d1a80be22008375e81056b63592c74c222
somsakk <88964214+somsakk@users.noreply.github.com>
2025-09-10 22:12:56 +0700

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 
 int fact(int n) {
+  int result;
   if (n <= 1) {
-    return 1;
+    result = 1;
   } 
   else {
-    return n*fact(n-1);
+    result = n*fact(n-1);
   }
+  return result;
 }
 
 int main(void) {
