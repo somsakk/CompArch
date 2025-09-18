5a5976607734f029de9845ba4763790f131686ac
somsakk <88964214+somsakk@users.noreply.github.com>
2025-09-18 13:12:57 +0700

    Update factorial2.c

--- factorial2.c
+++ factorial2.c
@@ -23,7 +23,7 @@ int fact_iterative(int n) {
 int main(void) {
   int a = 5;  // Overflow happens with a >= 28
   int f1, f2;
-  // f1 = fact_recursive(a);
+  f1 = fact_recursive(a);
   f2 = fact_iterative(a);
   printf("Recursive factorial of %d is %d\n", a, f1);
   printf("Iterative factorial of %d is %d\n", a, f2);
