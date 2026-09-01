f9e10c58de30d6ab3f2bfff3fd5021452a9c6eaa
somsakk <88964214+somsakk@users.noreply.github.com>
2026-09-01 14:26:28 +0700

    Create age.c

---
+++ age.c
@@ -0,0 +1,18 @@
+//age.c
+#include <stdio.h> // ต้อง include ไฟล์ stdio.h ทีมีมาแล้วในไลบรารี่ เพื่อใช้ printf() ได้
+
+int main(void) {
+    int age = 21;
+    float gpa = 3.75;
+    char grade = 'A';
+
+    printf("อายุ: %d\n", age);
+    // %d = integer
+    printf("GPA: %.2f\n", gpa);
+    // %.2f = float ทศนิยม 2 ตําแหน่ง
+    printf("เกรด: %c\n", grade);
+    // %c = char
+    printf("อายุ %d เกรด %c\n", age, grade); // ใส่หลายตัวได้
+
+    return 0;
+}
