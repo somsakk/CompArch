8e1a825a0117a64e3ede3bc621b0f955f0a7509e
somsakk <somsak.kit@dpu.ac.th>
2026-09-14 22:50:43 +0700

    update

--- array_pointer_hw.c
+++ array_pointer_hw.c
@@ -1,10 +1,10 @@
 // array_pointer_hw.c
 #include<stdio.h>
-#include<stdint.h>
+// #include<stdint.h>
 
 int main(void)
 {
-    int scores[5] = {-1, 789, -16, 1025, -1025};
+    int scores[5] = {-1, 16, -16, 258, -258};
     
     printf("scores[1] = %d = %#x\n", scores[1], scores[1]);
     printf("*scores = %d = %#x\n", *scores, *scores);
@@ -17,4 +17,9 @@ int main(void)
     printf("Address of scores[0] = %p\n", &scores[0]);
     printf("Address of scores[1] = %p\n", &scores[1]); 
     printf("Address of scores[4] = %p\n", &scores[4]); 
+
+	*(scores+2) = 3;
+	for (int i = 1; i<3; i++) {
+		printf("%d\n", scores[i]);
+	}
 }
---
+++ pointer_hw1.c
@@ -0,0 +1,28 @@
+// pointer_hw1.c
+#include<stdio.h>
+
+typedef struct contact {
+    char name[20];
+    int phone;
+    unsigned int age;
+} contact;
+
+int main(void)
+{
+    contact my_contacts[3];
+    
+    printf("Sizes: sizeof(contact)=%#x, sizeof(my_contacts)=%#x, sizeof(my_contacts[0])=%#x\n", 
+        (int)sizeof(contact), (int)sizeof(my_contacts), (int) sizeof(my_contacts[0]));
+    printf("Length of my_contacsts: %d\n", (int) sizeof(my_contacts)/sizeof(my_contacts[0]));
+
+    printf("Addresses of my_contacts[0]: %p\n", &my_contacts[0]);
+    printf("Addresses of my_contacts[1]: %p\n", &my_contacts[1]);
+    printf("Addresses of my_contacts[2]: %p\n", (my_contacts+2)); 
+	// since (my_contacts+2) == &my_contacts[2]
+     
+    printf("Adddress:\n");
+    for (int i=0; i<3; i++) {
+        printf("\t&my_contacts[%d]: %p, &name: %p, &phone: %p, &age: %p\n", 
+            i, (my_contacts+i), &(my_contacts[i].name), &(my_contacts[i].phone), &(my_contacts[i].age));  
+        }   
+}
